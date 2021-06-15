0df301bcdcb57a34bb4c11d79b95b6a92eff9d7f
cmstroe <cristina.stroe98@yahoo.com>
2021-06-15 19:01:25 +0200

    add benchmark model

---
+++ bert-finetune.py
@@ -0,0 +1,46 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+import pandas as pd
+
+
+raw_datasets = load_dataset('csv', data_files='train_causes.csv')['train'].train_test_split(test_size=0.1)
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
+
+def tokenize_function(examples):
+    return tokenizer(examples['text'], padding="max_length", truncation=True)
+
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+small_train_dataset = tokenized_datasets['train']
+small_eval_dataset = tokenized_datasets['test']
+
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
+
+training_args = TrainingArguments("test_trainer")
+
+trainer = Trainer(
+    model=model, args=training_args, train_dataset=small_train_dataset, eval_dataset=small_eval_dataset
+)
+
+trainer.train()
+
+metric = load_metric("accuracy")
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=small_train_dataset,
+    eval_dataset=small_eval_dataset,
+    compute_metrics=compute_metrics,
+)
+trainer.evaluate()
