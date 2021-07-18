62746b4d9ad83548944679372c0666cc82a81334
cmstroe <cristina.stroe98@yahoo.com>
2021-07-18 17:16:25 +0300

    ing funding

--- pet/modeling.py
+++ pet/modeling.py
@@ -10,6 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import ast
+import ipdb
 import json
 import os
 import random
--- pet/wrapper.py
+++ pet/wrapper.py
@@ -18,6 +18,7 @@ import json
 import jsonpickle
 import os
 from typing import List, Dict, Optional
+import ipdb
 
 import torch
 import torch.nn as nn
@@ -143,7 +144,8 @@ class TransformerModelWrapper:
         config_class = MODEL_CLASSES[self.config.model_type]['config']
         tokenizer_class = MODEL_CLASSES[self.config.model_type]['tokenizer']
         model_class = MODEL_CLASSES[self.config.model_type][self.config.wrapper_type]
-
+        
+        ipdb.set_trace()
         model_config = config_class.from_pretrained(
             config.model_name_or_path, num_labels=len(config.label_list), finetuning_task=config.task_name,
             cache_dir=config.cache_dir if config.cache_dir else None, use_cache=False)
